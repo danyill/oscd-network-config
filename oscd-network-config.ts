5a7eae7079a202b35d4ab17415e19f52b304c50e
danyill <danyill@users.noreply.github.com>
2023-11-01 03:00:41 +0000

    Lint

--- oscd-network-config.ts
+++ oscd-network-config.ts
@@ -498,18 +498,23 @@ export default class NetworkConfig extends LitElement {
 
         if (smvMacsIngress.length > 0)
           accessListsIn.push(
-            [`mac access-list extended ${macFilterInACL}`,
-            smvMacsIngress.map(mac => `  permit host ${mac} any`).join('\n'),
-            `  deny   any any`,
-            `!`].join('\n'))
+            [
+              `mac access-list extended ${macFilterInACL}`,
+              smvMacsIngress.map(mac => `  permit host ${mac} any`).join('\n'),
+              `  deny   any any`,
+              `!`
+            ].join('\n')
+          );
 
         if (smvMacsEgress.length > 0)
-        accessListsIn.push(
-            [`mac access-list extended ${macFilterOutACL}`,
-            smvMacsEgress.map(mac => `  permit host ${mac} any`).join('\n'),
-            `  deny   any any`,
-            `!`].join('\n'))
-          
+          accessListsIn.push(
+            [
+              `mac access-list extended ${macFilterOutACL}`,
+              smvMacsEgress.map(mac => `  permit host ${mac} any`).join('\n'),
+              `  deny   any any`,
+              `!`
+            ].join('\n')
+          );
 
         return `interface ${portInfo.portName}
   description ${this.substation} Protection ${this.protectionSystem} LAN ${
