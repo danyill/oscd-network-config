c46e8a1f3b1b928f11b903607fed1213bf4773ba
danyill <danyill@users.noreply.github.com>
2023-11-01 00:17:04 +0000

    wip

--- oscd-network-config.ts
+++ oscd-network-config.ts
@@ -197,23 +197,125 @@ const relayPortInformation: portData[] = [
 // Update versions
 
 const sampleData = `
-XAT111,GigabitEthernet1/0/1,XAT_BusA_P2
-XAT111,GigabitEthernet1/0/2,XAT_C3_P2
-XAT111,GigabitEthernet1/0/3,XAT_T1_P2
-XAT111,GigabitEthernet1/0/4,XAT_220_M2
-XAT111,GigabitEthernet1/0/5,XAT_232_M2
-XAT111,GigabitEthernet1/0/6,XAT_242_M2
-XAT111,GigabitEthernet1/0/7,XAT_252_M2
-XAT111,GigabitEthernet1/0/8,XAT_2202_M2
-XAT111,GigabitEthernet1/0/9,XAT_C3_M2
-XAT111,GigabitEthernet1/0/10,XAT_T1_M2
-XAT111,GigabitEthernet1/0/11,XAT_278_M2
-XAT111,GigabitEthernet1/0/12,XAT_232_P2
-XAT111,GigabitEthernet1/0/13,SOM_102_M2
-XAT111,GigabitEthernet1/0/14,SOM_102_P2
-XAT111,GigabitEthernet1/0/15,XAT_T1_M4
-XAT111,GigabitEthernet1/0/16,XAT_350_M2
-XAT111,GigabitEthernet1/0/17,XAT_288_M2
+   
+XATSWA051, GigabitEthernet1/0/1, XAT_P1_SMS01_SG4260_9002794, ENET1   
+XATSWA051, GigabitEthernet1/0/2, XAT_P2_SMS01_SG4260_9003816, ENET-1       
+XATSWA051, GigabitEthernet1/0/5, XATHMI001, LAN1   
+XATSWA051, GigabitEthernet1/0/6, KF832E, ETH1   
+XATSWA051, GigabitEthernet1/0/7, PF461Y, ETH1   
+XATSWA051, GigabitEthernet1/0/8, FK681L, ETH1   
+XATSWA051, GigabitEthernet1/0/9, BN278M, A   
+XATSWA051, GigabitEthernet1/0/10, FN652C, ETH1   
+XATSWA051, GigabitEthernet1/0/11, TEST_2, Front   
+XATSWA051, GigabitEthernet1/0/12, XAT_Temp_C1, Ethernet   
+XATSWA051, GigabitEthernet1/0/13, XATSWA101, GigabitEthernet1/0/24   
+XATSWA051, GigabitEthernet1/0/14, XATSWA102, GigabitEthernet1/0/28   
+XATSWA051, GigabitEthernet1/0/15, XATSWA052, GigabitEthernet1/0/15         
+XATSWA051, GigabitEthernet1/0/19, XAT-DSSSVR02, 10gePCIe3-1   
+XATSWA051, GigabitEthernet1/0/20, XATSWA111, GigabitEthernet1/0/24     
+XATSWA051, GigabitEthernet1/0/22, XATSWA112, GigabitEthernet1/0/24   
+XATSWA051, GigabitEthernet1/0/23, AC PDU, ETH   
+XATSWA051, GigabitEthernet1/0/24, XAT_TSYN_BusA_1, ETHF       
+XATSWA051, GigabitEthernet1/0/27, XATSWA052, GigabitEthernet1/0/27        
+XATSWA052, GigabitEthernet1/0/1, XAT_P1_SMS01_SG4260_9002794, ENET2   
+XATSWA052, GigabitEthernet1/0/2, XAT_P2_SMS01_SG4260_9003816, ENET-2       
+XATSWA052, GigabitEthernet1/0/5, XATHMI001, LAN2   
+XATSWA052, GigabitEthernet1/0/6, KF832E, ETH2   
+XATSWA052, GigabitEthernet1/0/7, PF461Y, ETH2   
+XATSWA052, GigabitEthernet1/0/8, FK681L, ETH2   
+XATSWA052, GigabitEthernet1/0/9, BN278M, B   
+XATSWA052, GigabitEthernet1/0/10, FN652C, ETH2   
+XATSWA052, GigabitEthernet1/0/11, DSS_LAB_Desk_Sw_1, GigabitEthernet0/1     
+XATSWA052, GigabitEthernet1/0/13, XATSWA211, GigabitEthernet1/0/28   
+XATSWA052, GigabitEthernet1/0/14, XATSWA212, GigabitEthernet1/0/28   
+XATSWA052, GigabitEthernet1/0/15, XATSWA051, GigabitEthernet1/0/15     
+XATSWA052, GigabitEthernet1/0/17, XAT-DSSLAP03, Toshiba KB Dock     
+XATSWA052, GigabitEthernet1/0/19, XAT-DSSSVR02, 10gePCIe6-1   
+XATSWA052, GigabitEthernet1/0/20, HAYSWA010, GigabitEthernet1/0/24   
+XATSWA052, GigabitEthernet1/0/21, XATSWA211, GigabitEthernet1/0/24   
+XATSWA052, GigabitEthernet1/0/22, XATSWA212, GigabitEthernet1/0/24   
+XATSWA052, GigabitEthernet1/0/23, XAT_TSYN_BusA_2, Admin   
+XATSWA052, GigabitEthernet1/0/24, BN278M, ETH   
+XATSWA052, GigabitEthernet1/0/25, XATSWA302, GigabitEthernet0/2     
+XATSWA052, GigabitEthernet1/0/27, XATSWA051, GigabitEthernet1/0/27   
+XATSWA052, GigabitEthernet1/0/28, XATSWO013, GigabitEthernet1/0/52      
+XATSWA101, GigabitEthernet1/0/1, XATSWA111, GigabitEthernet1/0/28                     
+XATSWA101, GigabitEthernet1/0/11, XAT_RTAC_2, ETH1     
+XATSWA101, GigabitEthernet1/0/13, DSSPOCEWS01, Prot1:PORT-0           
+XATSWA101, GigabitEthernet1/0/18, XAT_P1_SMS01_SG4260_9002794, C-ENET1     
+XATSWA101, GigabitEthernet1/0/20, XAT_P2_SMS01_SG4260_9003816, ENET-C1         
+XATSWA101, GigabitEthernet1/0/24, XATSWA051, GigabitEthernet1/0/13                                  
+XATSWA102, GigabitEthernet1/0/11, XAT_RTAC_2, ETH2   
+XATSWA102, GigabitEthernet1/0/12, XAT_RTAC_2, ETH3   
+XATSWA102, GigabitEthernet1/0/13, DSSPOCEWS01, Prot1:PORT-1           
+XATSWA102, GigabitEthernet1/0/18, XAT_P1_SMS01_SG4260_9002794, C-ENET2     
+XATSWA102, GigabitEthernet1/0/20, XAT_P2_SMS01_SG4260_9003816, ENET-C2                 
+XATSWA102, GigabitEthernet1/0/28, XATSWA051, GigabitEthernet1/0/14      
+XATSWA111, GigabitEthernet1/0/1, XAT_BusA_P1, a1   
+XATSWA111, GigabitEthernet1/0/2, XAT_232_P1, 1GB_PORT5A   
+XATSWA111, GigabitEthernet1/0/3, SOM_112_P1, 1GB_PORT5A   
+XATSWA111, GigabitEthernet1/0/4, XAT_T1_P1, 1GB_PROT5A   
+XATSWA111, GigabitEthernet1/0/5, XAT_242_M1 (ODJB), Port1   
+XATSWA111, GigabitEthernet1/0/6, XAT_T1_M3, PORT1A   
+XATSWA111, GigabitEthernet1/0/7, TEST, Port1   
+XATSWA111, GigabitEthernet1/0/8, XAT_242_M3, 1A   
+XATSWA111, GigabitEthernet1/0/9, XAT_BusA_P1, PRT2   
+XATSWA111, GigabitEthernet1/0/10, TEST_2, Port1   
+XATSWA111, GigabitEthernet1/0/11, XAT_242_C1, 1A                     
+XATSWA111, GigabitEthernet1/0/21, XAT_TSYN_BusA_1, ETH1   
+XATSWA111, GigabitEthernet1/0/22, XAT_TSYN_BusA_2, ETH3     
+XATSWA111, GigabitEthernet1/0/24, XATSWA051, GigabitEthernet1/0/20         
+XATSWA111, GigabitEthernet1/0/28, XATSWA101, GigabitEthernet1/0/1      
+XATSWA112, GigabitEthernet1/0/1, XAT_BusA_P1, b1   
+XATSWA112, GigabitEthernet1/0/2, XAT_232_P1, 1GB_PORT5B   
+XATSWA112, GigabitEthernet1/0/3, SOM_112_P1, 1GB_PORT5B   
+XATSWA112, GigabitEthernet1/0/4, XAT_T1_P1, 1GB_PROT5B   
+XATSWA112, GigabitEthernet1/0/5, XAT_242_M1 (ODJB), Port2   
+XATSWA112, GigabitEthernet1/0/6, XAT_T1_M3, PORT1B   
+XATSWA112, GigabitEthernet1/0/7, TEST, Port2   
+XATSWA112, GigabitEthernet1/0/8, XAT_242_M3, 1B   
+XATSWA112, GigabitEthernet1/0/9, XAT_BusA_P1, PRT3   
+XATSWA112, GigabitEthernet1/0/10, TEST_2, Port2   
+XATSWA112, GigabitEthernet1/0/11, XAT_242_C1, 1B           
+XATSWA112, GigabitEthernet1/0/16, XAT_TSYN_BusA_3, ETH1       
+XATSWA112, GigabitEthernet1/0/19, XAT_TSYN_BusA_3, ETH3     
+XATSWA112, GigabitEthernet1/0/21, XAT_TSYN_BusA_1, ETH2   
+XATSWA112, GigabitEthernet1/0/22, XAT_TSYN_BusA_2, ETH4     
+XATSWA112, GigabitEthernet1/0/24, XATSWA051, GigabitEthernet1/0/22              
+XATSWA211, GigabitEthernet1/0/1, XAT_BusA_P2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/2, XAT_232_P2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/3, SOM_112_P2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/4, XAT_T1_P2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/5, XAT_C3_P2, E:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/6, XAT_T1_M4, PORT1A   
+XATSWA211, GigabitEthernet1/0/7, XAT_278_M2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/8, XAT_2202_M2, F:ETH-BD-2FO:CH1   
+XATSWA211, GigabitEthernet1/0/9, XAT_P1_SMS01_SG4260_9002794, C-ENET3   
+XATSWA211, GigabitEthernet1/0/10, XAT_242_M2, PortF_ETH-BD-2FO_CH1   
+XATSWA211, GigabitEthernet1/0/11, DSSPOCEWS01, Prot2:PORT-0               
+XATSWA211, GigabitEthernet1/0/18, XAT_RTAC_2, ETH4       
+XATSWA211, GigabitEthernet1/0/21, XAT_TSYN_BusA_1, ETH3   
+XATSWA211, GigabitEthernet1/0/22, XAT_TSYN_BusA_2, ETH5     
+XATSWA211, GigabitEthernet1/0/24, XATSWA052, GigabitEthernet1/0/21   
+XATSWA211, GigabitEthernet1/0/25, XAT_P1_SMS01_SG4260_9002794, D-ENET1   
+XATSWA211, GigabitEthernet1/0/26, XAT_P2_SMS01_SG4260_9003816, ENET-C3     
+XATSWA211, GigabitEthernet1/0/28, XATSWA052, GigabitEthernet1/0/13      
+XATSWA212, GigabitEthernet1/0/1, XAT_BusA_P2, F:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/2, XAT_232_P2, F:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/3, SOM_112_P2, F:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/4, XAT_T1_P2, F:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/5, XAT_C3_P2, E:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/6, XAT_T1_M4, PORT1B   
+XATSWA212, GigabitEthernet1/0/7, XAT_278_M2, F:ETH-BD-2FO:CH2   
+XATSWA212, GigabitEthernet1/0/8, XAT_2202_M2, F:ETH-BD-2FO:CH2     
+XATSWA212, GigabitEthernet1/0/10, XAT_242_M2, PortF_ETH-BD-2FO_CH2   
+XATSWA212, GigabitEthernet1/0/11, DSSPOCEWS01, Prot2:PORT-1                     
+XATSWA212, GigabitEthernet1/0/21, XAT_TSYN_BusA_1, ETH4   
+XATSWA212, GigabitEthernet1/0/22, XAT_TSYN_BusA_2, ETH6     
+XATSWA212, GigabitEthernet1/0/24, XATSWA052, GigabitEthernet1/0/22   
+XATSWA212, GigabitEthernet1/0/25, XAT_P1_SMS01_SG4260_9002794, D-ENET2   
+XATSWA212, GigabitEthernet1/0/26, XAT_P2_SMS01_SG4260_9003816, ENET-C4     
+XATSWA212, GigabitEthernet1/0/28, XATSWA052, GigabitEthernet1/0/14   
 `;
 
 /**
@@ -241,7 +343,7 @@ export default class NetworkConfig extends LitElement {
   substation: string = '';
 
   @property({ attribute: false })
-  portData: { switchName: string; portName: string; iedName: string }[] | null =
+  portData: { switchName: string; portName: string; iedName: string, receivingPortName: string }[] | null =
     null;
 
   @property({ attribute: false })
@@ -267,9 +369,10 @@ export default class NetworkConfig extends LitElement {
     this.portData = lines.map(line => {
       const fields = line.split(',');
       return {
-        switchName: fields[0],
-        portName: fields[1],
-        iedName: fields[2]
+        switchName: fields[0]?.trim(),
+        portName: fields[1]?.trim(),
+        iedName: fields[2]?.trim(),
+        receivingPortName: fields[3]?.trim()
       };
     });
 
@@ -290,9 +393,9 @@ export default class NetworkConfig extends LitElement {
     );
 
     this.substation = selectedEthernetSwitch.slice(0, 3);
-    this.protectionSystem = <'1' | '2'>selectedEthernetSwitch.slice(3, 4);
+    this.protectionSystem = <'1' | '2'>selectedEthernetSwitch.slice(6, 7) === '1' ? '1' : '2';
     this.prpNetwork =
-      parseInt(selectedEthernetSwitch.slice(5, 6), 10) % 2 === 1 ? 'A' : 'B';
+      parseInt(selectedEthernetSwitch.slice(8, 9), 10) % 2 === 1 ? 'A' : 'B';
 
     console.log(
       'Ports',
@@ -306,13 +409,49 @@ export default class NetworkConfig extends LitElement {
       'Native VLAN',
       this.nativeVlanUI.value
     );
+    console.log('hi')
+
+    const otherVlans = ''
+
+    const iedPorts = Array.from(this.doc.querySelectorAll(':root > IED')).map(ied => ied.getAttribute('name'))
+
+    console.log(iedPorts)
+
+    const something = portsToConfigure.filter(portData => iedPorts.includes(portData.iedName)).map((portData) => {
+
+      const iedName = portData.iedName
+
+      const gsePublish = Array.from(this.doc.querySelectorAll(`:root > Communication > SubNetwork > ConnectedAP[iedName="${iedName}"] > GSE`))
+      const smvPublish = Array.from(this.doc.querySelectorAll(`:root > Communication > SubNetwork > ConnectedAP[iedName="${iedName}"] > SMV`))
+      const gseSubscribe = Array.from(this.doc.querySelectorAll(`:root > Communication > SubNetwork > ConnectedAP[iedName="${iedName}"] > Private[type="Transpower-GSE-Subscribe"]`))
+      const smvSusbcribe = Array.from(this.doc.querySelectorAll(`:root > Communication > SubNetwork > ConnectedAP[iedName="${iedName}"] > Private[type="Transpower-SMV-Subscribe"]`))
+
+      console.log(iedName)
+      console.log(gsePublish)
+      console.log(smvPublish)
+      console.log(gseSubscribe)
+      console.log(smvSusbcribe)
+
+      return `interface ${portData.portName}
+  description ${this.substation} Protection ${this.protectionSystem} LAN ${this.prpNetwork} to ${iedName} ${portData.receivingPortName} 
+  switchport trunk native vlan ${this.nativeVlanUI.value}
+  switchport trunk allowed vlan ${this.nativeVlanUI.value}, ${otherVlans}
+  switchport mode trunk
+  load-interval 30
+  spanning-tree portfast trunk
+  service-policy input pm-dss-prot-vlan-mark-in
+  service-policy output pm-dss-lan-out
+!`}).join('\n')
+  console.log(something)
+  this.outputUI.value = something
+
   }
 
   protected firstUpdated(): void {
     this.refreshInputData();
   }
 
-  importData(): void {}
+  importData(): void { }
 
   render(): TemplateResult {
     return html`<section>
@@ -323,28 +462,29 @@ export default class NetworkConfig extends LitElement {
         <md-outlined-text-field
           id="input"
           type="textarea"
-          label="Input CSV data in the form: Switch Name, Port Name, IED Name"
+          label="Input CSV data in the form: Switch Name, Port Name, IED Name, Receiving Port Name"
           value="${sampleData}"
           rows="100"
           @input=${() => {
-            this.refreshInputData();
-          }}
+        this.refreshInputData();
+      }}
         >
         </md-outlined-text-field>
         <md-outlined-button
           class="clippy"
           @click=${() => {
-            navigator.clipboard.readText().then(
-              // eslint-disable-next-line no-return-assign
-              pasteText => (this.inputUI.value = pasteText)
-            );
-          }}
+        navigator.clipboard.readText().then(
+          // eslint-disable-next-line no-return-assign
+          pasteText => (this.inputUI.value = pasteText)
+        );
+      }}
           >Paste
           <md-icon slot="icon">content_paste</md-icon>
         </md-outlined-button>
         <md-outlined-button class="clippy" @click=${() => {
-          this.importCsvUI.click();
-        }}
+        this.importCsvUI.click();
+        this.refreshInputData();
+      }}
           >Import
           <md-icon slot="icon">attach_file</md-icon>
         </md-outlined-button>
@@ -359,11 +499,11 @@ export default class NetworkConfig extends LitElement {
             required
           >
             ${this.switchNames.map(
-              switchName =>
-                html`<md-select-option value="${switchName}">
+        switchName =>
+          html`<md-select-option value="${switchName}">
                   <div slot="headline">${switchName}</div>
                 </md-select-option>`
-            )}
+      )}
           </md-filled-select>
           <md-outlined-text-field
             class="selection-item"
@@ -376,9 +516,9 @@ export default class NetworkConfig extends LitElement {
           <md-filled-button
             class="selection-item"
             @click=${() => {
-              if (!this.doc) return;
-              this.buildOutputConfiguration();
-            }}
+        if (!this.doc) return;
+        this.buildOutputConfiguration();
+      }}
             >Build Configuration
             <md-icon slot="icon">build</md-icon>
           </md-filled-button>
@@ -393,55 +533,54 @@ export default class NetworkConfig extends LitElement {
         <md-outlined-button
           class="clippy"
           @click=${() => {
-            navigator.permissions
-              .query({ name: 'clipboard-write' as any })
-              .then(result => {
-                if (result.state === 'granted' || result.state === 'prompt') {
-                  navigator.clipboard.writeText(this.outputUI?.value ?? '');
-                }
-              });
-          }}
+        navigator.permissions
+          .query({ name: 'clipboard-write' as any })
+          .then(result => {
+            if (result.state === 'granted' || result.state === 'prompt') {
+              navigator.clipboard.writeText(this.outputUI?.value ?? '');
+            }
+          });
+      }}
           >Copy
           <md-icon slot="icon">content_copy</md-icon>
         </md-outlined-button>
         <md-outlined-button
           class="clippy"
           @click=${() => {
-            const outputText = this.outputUI?.value ?? '';
+        const outputText = this.outputUI?.value ?? '';
 
-            const blob = new Blob([outputText], {
-              type: 'application/xml'
-            });
+        const blob = new Blob([outputText], {
+          type: 'application/xml'
+        });
 
-            const a = document.createElement('a');
-            a.download = `Network_Configuration_${
-              this.ethernetSwitchUI?.value ?? 'Unknown'
-            }.txt`;
-            a.href = URL.createObjectURL(blob);
-            a.dataset.downloadurl = [
-              'application/xml',
-              a.download,
-              a.href
-            ].join(':');
-            a.style.display = 'none';
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
+        const a = document.createElement('a');
+        a.download = `Network_Configuration_${this.ethernetSwitchUI?.value !== '' ? this.ethernetSwitchUI?.value : 'Unknown'
+          }.txt`;
+        a.href = URL.createObjectURL(blob);
+        a.dataset.downloadurl = [
+          'application/xml',
+          a.download,
+          a.href
+        ].join(':');
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
 
-            setTimeout(() => {
-              URL.revokeObjectURL(a.href);
-            }, 5000);
-          }}
+        setTimeout(() => {
+          URL.revokeObjectURL(a.href);
+        }, 5000);
+      }}
           >Download
           <md-icon slot="icon">download</md-icon>
         </md-outlined-button>
       </section>
       
       <input @click=${
-        // eslint-disable-next-line no-return-assign
-        (event: MouseEvent) =>
-          // eslint-disable-next-line no-param-reassign
-          ((<HTMLInputElement>event.target).value = '')
+      // eslint-disable-next-line no-return-assign
+      (event: MouseEvent) =>
+        // eslint-disable-next-line no-param-reassign
+        ((<HTMLInputElement>event.target).value = '')
       } @change=${async (event: Event) => {
         const file =
           (<HTMLInputElement | null>event.target)?.files?.item(0) ?? false;
